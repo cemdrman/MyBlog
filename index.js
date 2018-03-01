6844b65c4f0c26ccb6787000dae7eec435c042ec
cem dırman <cemdrman@gmail.com>
2018-03-01 14:32:13 +0300

    index route

---
+++ index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express.express();
+
+
+
+//---Routes---
+const indexRoutes = require("./routes/indexRoutes");
+
+
+
+
+
+
+//Routes Using
+app.use(indexRoutes);
+
+
+
+
+
+
+//---App Config
+//view engine
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+
+
+//---Server
+
+const server = app.listen(3000,() =>{
+    console.log("App started");
+});
