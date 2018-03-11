2cb8ca30cf02fb5149ec12541cfbcabe1f54d674
cem dırman <cemdrman@gmail.com>
2018-03-12 00:07:04 +0300

    modified routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,10 +6,11 @@ router.get("/admin/singin", (req, res) => {
     res.render('./admin/singin');
 });
 
-
-router.post("/singin",(req,res) =>{
-   let email = req.body.email;
-   let password = req.body.password;
-   console.log(email + " " + password); 
+router.post("/admin_singin", (req, res) => {
+    res.render('./admin/admin_dashboard');
+   
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log(email + " " + password);
 });
 module.exports = router;
