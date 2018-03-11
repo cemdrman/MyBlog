27bf0bd997d1fc7d3a19c4bdc24d749ec4d9b14a
cem dırman <cemdrman@gmail.com>
2018-03-12 00:07:33 +0300

    v 1.0

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const user = require("./models/userModel");
+//const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-
-
 //---Routes---
 const indexRoutes = require("./routes/indexRoutes");
-
-
-
-
+const adminRoutes = require("./routes/adminRoutes");
 
 
 //Routes Using
 app.use(indexRoutes);
-
-
-
+app.use(adminRoutes);
 
 
 
 //---App Config
+mongoose.connect("mongodb://localhost/MyBlogApp");
+
 //view engine
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+/*app.use(require('express-session')({
+    secret :"secret-keyword",
+    resave:false,
+    saveUninitialized:false
+}));
 
-
+app.use(passport.initialize);
+app.use(passport.session());
+passport.use(new localStrategy(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+*/
 
 //---Server
 
-const server = app.listen(3000,() =>{
+const server = app.listen(3000, () => {
     console.log("App started");
 });
