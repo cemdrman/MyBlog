8370ddc4778d57e50ce7a93772fe898b86979788
cem dırman <cemdrman@gmail.com>
2018-07-04 20:05:57 +0300

    changed model names

--- models/User.js
+++ models/User.js
@@ -1,13 +1,12 @@
 const mongoose =  require('mongoose');
-
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = mongoose.Schema({
-    userEmail : String,
-    userPassword : String
+    email : String,
+    password : String
 });
 
 userSchema.plugin(passportLocalMongoose);
 
 //UserSchema modelini disari export ediyoruz, adi ise User
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
