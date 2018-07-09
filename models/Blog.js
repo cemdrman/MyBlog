c8be27af97fcc3cf349fd80b06643ca2b29d5401
cem dırman <cemdrman@gmail.com>
2018-07-10 02:49:23 +0300

    updated blog properties

--- models/Blog.js
+++ models/Blog.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const blogSchema = mongoose.Schema({
     blogTitle : { type: String, required: "Can not be empty"},
-    blogImage : { type: String, required: "Can not be empty"},
-    blog      : { type: String, required: "Can not be empty"},
+    blogImage : { type: String},
+    blog      : { type: String},
     blogDate  : { type: Date,default: Date.now}
 });
 
