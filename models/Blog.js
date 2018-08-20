02efd9cb70fa34a6a8622c272e60e09806639252
cem dırman <cemdrman@gmail.com>
2018-08-21 01:00:22 +0300

    modified

--- models/Blog.js
+++ models/Blog.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const blogSchema = mongoose.Schema({
-    blogTitle : { type: String, required: "Can not be empty"},
-    blogImage : { type: String},
-    blog      : { type: String},
-    blogDate  : { type: Date,default: Date.now}
+    blogTitle : { type: String, required: "Title can not be empty"},
+    blogImage : { type: String, required: "Image can not be empty"},
+    blog      : { type: String, required: "Blog message can not be empty"},
+    blogDate  : { type: Date, default: Date.now}
 });
-
 module.exports = mongoose.model("Blog",blogSchema);
