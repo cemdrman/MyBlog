9893f99d0c6d4a85b8a56739e39a2738c29426d0
cem dırman <cemdrman@gmail.com>
2018-10-10 21:04:39 +0300

    server port changed

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ passport.deserializeUser(user.deserializeUser());
 */
 
 //---Server
-
-const server = app.listen(process.env.PORT, () => {    
+var port = process.env.PORT || 8080;
+const server = app.listen(port, () => {  
+    console.log('Our app is running on http://localhost:' + port);  
 });
